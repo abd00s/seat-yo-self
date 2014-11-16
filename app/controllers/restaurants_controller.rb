57c1c0dc059ee53aa995433aabd7cbb8d788d50d
Abdullah <abd00s@users.noreply.github.com>
2014-11-16 14:39:49 -0500

    create restaurants controller and views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def welcome
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
