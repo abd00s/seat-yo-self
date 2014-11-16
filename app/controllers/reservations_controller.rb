6cd8c1a2edbb9c914355112e96ca3cff25672658
Abdullah <abd00s@users.noreply.github.com>
2014-11-16 18:17:33 -0500

    reso routes and controller actions done

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,53 @@
+class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.new
+  end
+
+  def edit
+    
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.new(reservation_params)
+    if @reservation.save
+      redirect_to restaurant_path(@restaurant), notice: 'Booking Successfull'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @reservation.update_attributes(reservation_params)
+      redirect_to reservation_path(@reservation)
+    else
+      render :edit
+    end     
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path    
+  end
+
+  private
+  def set_reservation
+    @reservation = Reservation.find(params[:id])  
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:restaurant_id, :user_id, :date, :time, :size)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+
   resources :users
-  resources :restaurants
+  resources :restaurants do
+    resources :reservations
+  end
+
   root 'restaurants#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
