faee48b4e320ab7ae3df9edecb743350884b0373
Abdullah <abd00s@users.noreply.github.com>
2014-11-17 21:34:36 -0500

    def available in rest model, reurn value is correct

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,10 @@ class Restaurant < ActiveRecord::Base
 	has_many :customers, through: :reservations
   belongs_to :owner
 
-
   validates :name, :address, :description, presence: true
+
+  def available?(size,time)
+    reserved = reservations.where(time: time).sum(:size)
+    size <= (capacity - reserved)
+  end
 end
