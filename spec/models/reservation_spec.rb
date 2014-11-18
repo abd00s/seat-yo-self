e283c1663d22c3450c3cc659e9c55de973349b9b
Abdullah <abd00s@users.noreply.github.com>
2014-11-17 21:53:30 -0500

    ghetto debug passes, test doesnt, prob datetime issues

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -16,5 +16,21 @@ describe Restaurant do
     it "should return true if there's exactly the capacity of the restaurant" do
       expect(restaurant.available?(100, time)).to be(true)
     end
+
+    context "with one existing reservation" do
+      let!(:reservation) { Reservation.create(:restaurant => restaurant, :time => time, :size => 10)}
+
+      it "should return true if there's space available" do
+        expect(restaurant.available?(10, time)).to be(true)
+      end
+
+      it "should return false if there's no space available" do
+        expect(restaurant.available?(99, time)).to be(false)
+      end
+
+      # it "should return true if the reservation is for a different time" do
+      #   expect(restaurant.available?(99, start_time + 1.hour)).to be(true)
+      # end
+    end    
   end
 end
