b0ac428ba02eed7e3a146c0dd4503a8c9f0e61ee
Abdullah <abd00s@users.noreply.github.com>
2014-11-17 22:07:01 -0500

    cant make reservation if it exceed capacity, in views

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :customer
 	belongs_to :restaurant
+
+  validate :availability
+
+  private
+  def availability
+    unless restaurant.available?(size, time)
+      errors.add(:base, "Restaurant is full at this hour")
+    end
+  end
 end
