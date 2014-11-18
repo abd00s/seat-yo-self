e8fe580eeeadbbd109d06c8e68c652e9b6b98c78
Abdullah <abd00s@users.noreply.github.com>
2014-11-17 21:44:14 -0500

    tests pass when theres no existing resos

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 describe Restaurant do
+  describe "#available?" do 
+    let(:time) {DateTime.new(2014,11,25,17,0)}
+    let(:restaurant) {Restaurant.create(name: "Canoe", capacity: 100)}
 
+    it "should return true if there's space available" do
+      expect(restaurant.available?(10,time)).to be(true)
+    end
 
+    it "should return false if there's no space available" do
+      expect(restaurant.available?(200, time)).to be(false)
+    end
+    
+    it "should return true if there's exactly the capacity of the restaurant" do
+      expect(restaurant.available?(100, time)).to be(true)
+    end
+  end
 end
